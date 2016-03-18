cb8e6c5dd36713813f2bf80adf6daf4045b96d6a
Ned Twigg <ned.twigg@diffplug.com>
2016-03-18 03:31:48 -0700

    A basic implementation of Rebase, Revert, and CherryPick.

---
+++ org.eclipse.jgit.pgm/src/org/eclipse/jgit/pgm/Revert.java
@@ -0,0 +1,87 @@
+/*
+ * Copyright (C) 2016, Ned Twigg <ned.twigg@diffplug.com>
+ * and other copyright owners as documented in the project's IP log.
+ *
+ * This program and the accompanying materials are made available
+ * under the terms of the Eclipse Distribution License v1.0 which
+ * accompanies this distribution, is reproduced below, and is
+ * available at http://www.eclipse.org/org/documents/edl-v10.php
+ *
+ * All rights reserved.
+ *
+ * Redistribution and use in source and binary forms, with or
+ * without modification, are permitted provided that the following
+ * conditions are met:
+ *
+ * - Redistributions of source code must retain the above copyright
+ *   notice, this list of conditions and the following disclaimer.
+ *
+ * - Redistributions in binary form must reproduce the above
+ *   copyright notice, this list of conditions and the following
+ *   disclaimer in the documentation and/or other materials provided
+ *   with the distribution.
+ *
+ * - Neither the name of the Eclipse Foundation, Inc. nor the
+ *   names of its contributors may be used to endorse or promote
+ *   products derived from this software without specific prior
+ *   written permission.
+ *
+ * THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND
+ * CONTRIBUTORS "AS IS" AND ANY EXPRESS OR IMPLIED WARRANTIES,
+ * INCLUDING, BUT NOT LIMITED TO, THE IMPLIED WARRANTIES
+ * OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE
+ * ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT OWNER OR
+ * CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL,
+ * SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING, BUT
+ * NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES;
+ * LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER
+ * CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT,
+ * STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE)
+ * ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF
+ * ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+ */
+
+package org.eclipse.jgit.pgm;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import org.eclipse.jgit.api.Git;
+import org.eclipse.jgit.api.RevertCommand;
+import org.eclipse.jgit.lib.ObjectId;
+import org.eclipse.jgit.revwalk.RevCommit;
+import org.kohsuke.args4j.Argument;
+import org.kohsuke.args4j.Option;
+
+/**
+ * Implementation of git cherry-pick, but only the non-committing king
+ */
+@Command(common = true, usage = "usage_revert")
+public class Revert extends TextBuiltin {
+	@Argument(required = true, metaVar = "metaVar_commitish")
+	private List<String> commits = new ArrayList<String>();
+
+	@Option(name = "--no-commit", aliases = { "-n" }, usage = "usage_NoCommit")
+	private boolean noCommit;
+
+	@Override
+	protected void run() throws Exception {
+		try (Git git = new Git(db)) {
+			RevertCommand revert = git.revert();
+			for (String commit : commits) {
+				revert.include(ObjectId.fromString(commit));
+			}
+			revert.setNoCommit(noCommit);
+
+			RevCommit result = revert.call();
+			if (result != null) {
+				Rebase.success(outw);
+			} else {
+				throw Rebase.error(errw,
+						revert.getFailingResult().getMergeStatus(),
+						revert.getFailingResult().getCheckoutConflicts(),
+						revert.getFailingResult().getFailingPaths());
+			}
+		}
+	}
+}
